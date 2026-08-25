4e779a2bf7fab240963943872942e449c9cdc51f
Aryan Gupta <77137736+avy9999@users.noreply.github.com>
2026-08-25 23:16:53 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (24.36%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set <int> st(nums.begin(), nums.end());
+
+        int c = k;
+        while(st.count(c)){
+            c += k;
+        }
+        return c;
+    }
+};
