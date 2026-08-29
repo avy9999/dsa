ca451b707dde6a84770bdb846f3961fbac072384
Aryan Gupta <77137736+avy9999@users.noreply.github.com>
2026-08-29 13:32:05 +0530

    Time: 1760 ms (14.60%) | Memory: 9.2 MB (94.82%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -9,22 +9,20 @@ public:
         return true;
     }
 
-    int checkPartition(int i, int n, string &s, vector <int>& dp){
-        if (i == n) return 0;
-        if (dp[i] != -1) return dp[i];
-        int minCost = INT_MAX;
-        for (int j = i; j < n; ++j){
-            if (palindrome(s, i, j)){
-                int cost = 1 + checkPartition(j + 1, n, s, dp);
-                minCost = min(minCost, cost);
-            }
-        }
-        return dp[i] = minCost;
-    }
-
     int minCut(string s) {
         int n = s.size();
-        vector <int> dp(n, -1);
-        return checkPartition(0, n, s, dp) - 1;
+        vector <int> dp(n + 1, 0);
+        dp[n] = 0;
+        for (int i = n - 1; i >=0; i--){
+            int minCost = INT_MAX;
+            for (int j = i; j < n; ++j){
+                if (palindrome(s, i, j)){
+                    int cost = 1 + dp[j + 1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
     }
 };
